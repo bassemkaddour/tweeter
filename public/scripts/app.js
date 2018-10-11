579c19b9c960e40d7836f95d688bee33118ba2f6
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-11 01:20:11 +0000

    Add collapse button functionality and animations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).ready(function() {
+  $('.new-tweet').slideToggle(0);
 
   function createTweetElement(tweet) {
 
@@ -34,19 +36,21 @@ $(document).ready(function() {
 
     const $container = $("<article>").addClass('tweets').append($header).append($main).append($footer);
 
-    // console.log(container);
     return $container;
   }
 
   function renderTweets(tweetArray) {
+    $('#tweet-container').empty();
     const container = $('#tweet-container');
     for (const tweet of tweetArray) {
-      container.append(createTweetElement(tweet));
+      container.prepend(createTweetElement(tweet));
     }
   }
 
   function loadTweets() {
-    $.ajax('/tweets', {method: 'GET'}).then((tweetData) => {
+    $.ajax('/tweets', {
+      method: 'GET'
+    }).then((tweetData) => {
       renderTweets(tweetData)
     });
   }
@@ -55,93 +59,39 @@ $(document).ready(function() {
 
   $('.new-tweet form').on('submit', (e) => {
     e.preventDefault();
+    $('.error').hide();
+
     if ($('.tweet-text').val().length > 140) {
-      alert("Please keep the message less than 140 characters.");
+      $('.error').text('Please keep it under 140 characters.').slideDown('slow');
       return;
-    } else if ($('.tweet-text').val().length === 0){
-      alert("Please enter a tweet.");
+    } else if ($('.tweet-text').val().length === 0) {
+      $('.error').text('Please enter a tweet').slideDown('slow');
+
+      // $('.error')
+      //   .slideUp('fast', function() {
+      //     $(this).text('Please enter a tweet.')
+      //   }).slideDown('slow') ;
       return;
     }
+
     let data = $(e.target).serialize();
-    $.ajax('/tweets', { method: 'POST',
-                  data: data
+
+    $.ajax('/tweets', {
+      method: 'POST',
+      data: data
     }).then(() => {
-      $('#tweet-container').empty();
       $('.tweet-text').val("");
+      $('.counter').text(140);
+      $('.new-tweet').slideToggle('slow');
       loadTweets();
     });
   });
 
+
+  $('.compose-button').on('click', (e) => {
+    $('.new-tweet').slideToggle(550);
+    $('.tweet-text').focus();
+  });
+
 });
 
-  // // Test / driver code (temporary). Eventually will get this from the server.
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": {
-  //       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //     },
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // }
-
-  // // Fake data taken from tweets.json
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-
-  // renderTweets(data);
-  // // var $tweet = createTweetElement(tweetData);
-
-  // // // Test / driver code (temporary)
-  // // console.log($tweet); // to see what it looks like
-  // // $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
